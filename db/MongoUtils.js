84085fe39b8d29a76edb8ca23bffca1d415f6417
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2020-03-05 13:39:03 -0500

    Mongo changed

--- db/MongoUtils.js
+++ db/MongoUtils.js
@@ -1,13 +1,51 @@
 // Code taken from https://stackoverflow.com/questions/16571021/how-to-list-all-mongodb-databases-in-node-js/60546171#60546171
 const MongoClient = require("mongodb").MongoClient;
 
-const url = "mongodb://localhost:27017/";
-const client = new MongoClient(url, { useUnifiedTopology: true }); // useUnifiedTopology removes a warning
-
-
 // Connect
-function MongoUtils () {
-  const mu={};
+function MongoUtils() {
+  const mu = {};
+
+  mu.connect = () => {
+    const client = new MongoClient(
+      "mongodb+srv://vaca:vaca123@cluster0-3lhwp.mongodb.net/test?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true
+      }
+    );
+    return client.connect();
+  };
+
+  mu.databases = {};
+
+  mu.databases.list = () => {
+    mu.connect()
+      .then(
+        client =>
+          client
+            .db()
+            .admin()
+            .listDatabases() // Returns a promise that will resolve to the list of databases
+      )
+      .then(dbs => {
+        console.log("Mongo databases", dbs);
+      })
+      .finally(() => client.close()); // Closing after getting the data
+  };
+
+  mu.collections = {};
+
+  mu.collections.list = dbName => {
+    mu.connect()
+      .then(
+        client =>
+          client
+            .db(dbName)
+            .listCollections()
+            .toArray() // Returns a promise that will resolve to the list of the collections
+      )
+      .then(cols => console.log("Collections", cols))
+      .finally(() => mu.close());
+  };
 
   return mu;
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
+const mu = require("../db/MongoUtils.js");
+
 /* GET home page. */
 router.get("/", function(req, res) {
-  res.render("index", { title: "Express" });
+  mu.databases.list().then(databases =>
+    res.render("index", {
+      databases
+    })
+  );
 });
 
 module.exports = router;
